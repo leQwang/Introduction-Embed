420195d8899f5285a4bcd7ff3767b7166978cabb
leQwang <s3912105@rmit.edu.vn>
2023-09-27 22:09:00 +0700

    refactor Q3

--- Q3/Q3.ino
+++ Q3/Q3.ino
@@ -6,7 +6,7 @@ volatile bool isPressed = false;
 volatile int numberOfInterrupt = 0;
 
 void setup() {
-  Serial.begin(9600);  // Testing
+  Serial.begin(9600);
 
   EICRA |= (1 << ISC11);  // Falling edge triggers interrupt
   EIMSK |= (1 << INT1);   // Enable external interrupt INT1
@@ -21,7 +21,6 @@ void setup() {
   OCR1A = delayTime;                    // Set OCR1A for a 100 ms delay
 
   TIMSK1 = 0;  //turn off Timer 1 Interrupt
-  // TIMSK1 = (1 << OCIE1A); //Turn on Timer 1 Interrupt
 
   sei();  // Gloval interrupt enable
 
@@ -31,30 +30,24 @@ void setup() {
 
 ISR(INT1_vect) {
   Serial.println("---------Button Pressed---------");
-  isPressed = true;
-  TIMSK1 |= (1 << OCIE1A);
-  TCNT1=0;
+  isPressed = true;  //set button pressed to true
+  TIMSK1 |= (1 << OCIE1A);  //turn on timer
+  TCNT1=0;  //reset counter
 }
 
 
 ISR(TIMER1_COMPA_vect) {
   if (isPressed) {
     if (numberOfInterrupt == 0) {
-      Serial.print("Turn on Green Led at interrupt ");  // Testing
-      Serial.println(numberOfInterrupt);
-      PORTB |= (1 << 0);
+      PORTB |= (1 << 0);  //turn on the Led A
     } else if (numberOfInterrupt == 33) {
-      Serial.print("Turn off Green Led at interrupt ");  // Testing
-      Serial.println(numberOfInterrupt);
-      PORTB &= ~(1 << 0);
-      isPressed = false;
+      PORTB &= ~(1 << 0);  //turn off the Led A
+      isPressed = false; 
       TIMSK1 = 0;  //turn off Timer
     }
 
     if (numberOfInterrupt >= 1 && numberOfInterrupt < 33) {
-      Serial.print("GPIO2 interrupt number ");  // Testing
-      Serial.println(numberOfInterrupt);
-      PORTB ^= (1 << 1);
+      PORTB ^= (1 << 1);  //toggle Led B
     }
 
     if (numberOfInterrupt + 1 <= 33) {
